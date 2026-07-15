31b454fa4bf92a2a932bbe261d5cd861822bc6a8
Wangkech <wangkechgathothgatkuoth@gmail.com>
2026-07-15 11:04:51 +0300

    Made the switchPlayer method

--- app.js
+++ app.js
@@ -26,12 +26,26 @@ function Game(player1, player2) {
   }
   const board = gameBoard();
   const players = createPlayers(player1, player2);
+  const initialPlayer = players[0];
+  let currentPlayer = initialPlayer;
   return {
     players,
     board,
+    currentPlayer,
+
+    switchPlayer() {
+      let nextPlayer;
+      if (this.players.indexOf(this.currentPlayer) === 0) {
+        nextPlayer = this.players[1];
+      } else {
+        nextPlayer = this.players[0];
+      }
+
+      this.currentPlayer = nextPlayer;
+    },
   };
 }
 
 const game = Game("wangkech", "kelly");
 
-console.log(game.players);
+console.log(game.currentPlayer);
